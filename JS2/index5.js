5ecf06f39780d37cc5f8e103cbcd778600007f05
backup-de-proj <136359683+backup-de-proj@users.noreply.github.com>
2023-06-13 19:42:07 -0300

    Create index5.js

---
+++ JS2/index5.js
@@ -0,0 +1,41 @@
+function linha() {
+    console.log('----------------------------')
+}
+
+
+// ----------------------------
+linha()
+// -------------------------------
+
+
+function soma(a, b) {
+    return a + b
+}
+
+var total = soma(1, 2);
+console.log(total);
+
+
+// -------------------------------
+linha()
+// -------------------------------
+
+
+function RealDolar(real, dolar) {
+    return real * dolar
+}
+
+var resul = RealDolar(9, 5.12);
+console.log(resul);
+
+
+// -------------------------------
+linha();
+// -------------------------------
+
+
+function ola() {
+    console.log('olá');
+}
+
+ola();
