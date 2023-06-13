fdee10721329c09bb27a40943ebd6a756e809231
backup-de-proj <136359683+backup-de-proj@users.noreply.github.com>
2023-06-13 19:43:06 -0300

    Create index8.js

---
+++ JS2/index8.js
@@ -0,0 +1,23 @@
+const comidas = [
+    'arroz',
+    'feijao',
+    'macarrão',
+    'leite'
+];
+let teste = comidas;
+comidas.push('frango');
+console.log(teste);
+
+
+// console.log(comidas[0]);
+
+
+
+// const array2 [
+
+// ];
+// array2[0] = 'teste'
+
+
+
+// const array3 = new Array
