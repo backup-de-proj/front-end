f13ddbba2ba6d0ea58646923d7e79f02819672d6
backup-de-proj <136359683+backup-de-proj@users.noreply.github.com>
2023-06-13 19:41:49 -0300

    Create index4.js

---
+++ JS2/index4.js
@@ -0,0 +1,49 @@
+// var pote = 'Bombom';
+// console.log(pote);
+
+
+// var n1 = '1'
+// var n2 = '2'
+// var n3 = n1 + ' ' + n2
+// console.log(n3)
+
+
+// var n1 = '1'
+// var n2 = '2'
+// var n3 = n1 + n2
+// console.log(n3) // contatenação
+
+
+// var n1 = 1
+// var n1 = 2 // VAR pode ser redeclarada
+
+
+// let n2 = 1
+// let n2 = 2 // LET não pode ser redeclarada
+
+
+
+// var x = 10; // aqui é 10
+
+// {
+//     var x = 2; // aqui é 2
+// }
+// // aqui também é 2
+
+
+
+// let x = 10; // aqui é 10
+
+// {
+//     let x = 2; // SÓ AQUI É 2
+// }
+// // aqui também é 10
+
+
+
+// const x = 10; // aqui é 10
+
+// {
+//     const x = 2; // aqui é 10
+// }
+// const x = 12; // aqui também é 10 ( ERROR )
