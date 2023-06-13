08f46baf47af9be3615b8b239a92fcd7370ca144
backup-de-proj <136359683+backup-de-proj@users.noreply.github.com>
2023-06-13 19:45:34 -0300

    Create index13-1.js

---
+++ JS2/index13-1.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    console.log('6 segundos se passaram');
+}, 6000)
+
+
+
+var help = setInterval(function() {
+    // console.log('teste');
+}, 6000)
+
+clearInterval(help)
