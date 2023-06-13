a87f8cd12661fecb10bbc173a0925081131f9049
backup-de-proj <136359683+backup-de-proj@users.noreply.github.com>
2023-06-13 19:42:27 -0300

    Create index6.js

---
+++ JS2/index6.js
@@ -0,0 +1,16 @@
+const carro = {
+
+    marca: 'ford', 
+    modelo: 'ka', 
+    ano: '2015', 
+    placa: '1A44-67PpX',
+    buzina: function() {console.log('piiiiiiiiiiiiiiiiiiiiiiiiiii')},
+    // completo: function() {
+    //     return 'A marca é '+this.marca+' e o modelo é: '+this.modelo+
+    // }
+};
+
+// console.log(carro)
+// console.log(carro.ano)
+// carro.buzina()
+console.log(carro.completo)
