281a95e4f830d29cbd44b553e2919a926954bf86
backup-de-proj <136359683+backup-de-proj@users.noreply.github.com>
2023-06-13 19:44:42 -0300

    Create index11.js

---
+++ JS2/index11.js
@@ -0,0 +1,18 @@
+let num = 2;
+
+switch (num) {
+    case 1:
+        console.log('1');
+        break
+    case 2:
+        console.log('2');
+        break
+    
+    default:
+        console.log('nenhum dos dois');
+};
+
+var teste = 'OI';
+
+teste = teste.toLocaleLowerCase();
+console.log(teste)
